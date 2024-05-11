12955414c1577ba60e75368a880ddade369b9a36
rubenfb23 <rubenfernandezboullon@gmail.com>
2024-05-11 18:48:09 +0200

    Updated .gitignore and added code documentation

--- src/main.py
+++ src/main.py
@@ -9,9 +9,19 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 from langchain_community.document_loaders.csv_loader import CSVLoader
+import sys
 
 
 def receive_user_input():
+    """
+    Receives user input from the command line arguments and returns it as a string.
+
+    If command line arguments are provided, it concatenates them into a single string
+    and returns it. Otherwise, it returns a default prompt text.
+
+    Returns:
+        str: The user input as a string.
+    """
     if len(sys.argv) > 1:
         prompt_text = ' '.join(sys.argv[1:])
     else:
@@ -20,6 +30,16 @@ def receive_user_input():
 
 
 def load_a_website(website_url):
+    """
+    Loads a website and returns the loaded documents.
+
+    Args:
+        website_url (str): The URL of the website to load.
+
+    Returns:
+        list: A list of loaded documents.
+
+    """
     loader = WebBaseLoader(website_url)
     docs = loader.load()
     print("Loaded website")
@@ -27,20 +47,33 @@ def load_a_website(website_url):
 
 
 def load_a_csv(csv_path):
+    """
+    Load a CSV file and return the loaded documents.
+
+    Args:
+        csv_path (str): The path to the CSV file.
+
+    Returns:
+        list: A list of loaded documents.
+
+    """
     loader = CSVLoader(csv_path)
     docs = loader.load()
     print("Loaded CSV")
     return docs
 
 
+# Choose the model you want to use for LLM
 llm = Ollama(model="llama3:instruct")
 
 output_parser = StrOutputParser()
 
+# Choose the model you want to use for embeddings
 embeddings = OllamaEmbeddings(
                 model="nomic-embed-text",
             )
 
+# Choose the text splitter you want to use
 text_splitter = RecursiveCharacterTextSplitter(
     separators=[
         "\n\n",
@@ -60,37 +93,48 @@ text_splitter = RecursiveCharacterTextSplitter(
     length_function=len,
     is_separator_regex=False,)
 
-docs = load_a_website("https://es.wikipedia.org/wiki/Seann_William_Scott")
+# Load the documents from a website
+docs = load_a_website("https://www.who.int/es/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health")
 
+# Load the documents from a CSV file
 documents = text_splitter.split_documents(docs)
 print("Document splitted")
 
+# Create a vector store
 vector = FAISS.from_documents(documents, embeddings)
 print("Vector created")
 
+# Save the vector store
+vector.save_local("vector_store")
+print("Vector saved")
 
+# Load the vector store
 retriever = vector.as_retriever()
 print("Retriever created")
 
+# Create a prompt
 prompt = ChatPromptTemplate.from_template(
-"""Respond only in spanish. You are called AutoPaperwork, first of all, present yourself with shortly. Read the context carefully and with attention. Then, do what the input tells you to do based only on the context:
+"""Respond only in spanish. You are called AutoPaperwork and you are an expert on air quality, first of all, present yourself shortly. Read the context carefully and with attention. Then, do what the input tells you to do based only on the context:
 
 <context>
 {context}
 </context>
 
 Question: {input}""")
-print("Prompt created")
 
+# Create the document chain
 document_chain = create_stuff_documents_chain(llm, prompt)
 print("Document chain created")
 
+# Create the retrieval chain
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 print("Retrieval chain created")
 
+# Receive user input
 prompt_text = receive_user_input()
 print("Prompt text:")
 
+# Invoke the retrieval chain
 response = retrieval_chain.invoke({"input": prompt_text})
 print("Response:")
 print(response["answer"])
